24712abab3f75f79ea17fae4279a19fc9e0867fd
Guilherme-Jardim <guilhermejardimj@gmail.com>
2023-11-04 11:56:53 -0300

    sidebar

--- app/components/sidenav/GSideNav.tsx
+++ app/components/sidenav/GSideNav.tsx
@@ -1,25 +1,56 @@
 'use client'
-import { AppBar, Avatar, Box, Icon, List, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography, useTheme } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  useTheme
+} from '@mui/material';
 import Drawer from '@mui/material/Drawer';
 import { ReactNode } from 'react';
 import Divider from '@mui/material/Divider';
 import HomeIcon from '@mui/icons-material/Home';
-import { useMediaQuery } from '@material-ui/core';
-
+import { useMediaQuery } from '@mui/material';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 interface GSideNavProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
 export function GSideNav({ children }: GSideNavProps) {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
   return (
     <>
       <AppBar position="fixed" sx={{ height: theme.spacing(8), zIndex: 2000 }}>
+        <Toolbar>
+          <IconButton onClick={toggleDrawer}>
+            {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </Toolbar>
       </AppBar>
       <Box display='flex'>
-        <Drawer open={true} variant={smDown ? 'persistent' : 'permanent'}>
+        <Drawer variant='persistent' anchor="left" open={open} onClose={toggleDrawer}
+          sx={{
+            transition: 'margin 225ms cubic-bezier(0.4, 0, 0.6, 1)',
+          }}
+        >
+
+          <Box display='flex' alignItems='center' justifyContent='flex-end' padding={theme.spacing(0, 1)}>
+          </Box>
           <Box paddingTop={7} width={theme.spacing(28)} height='100%' display='flex' flexDirection='column'>
             <Box width="100%" height={theme.spacing(20)} display='flex' alignItems='center' justifyContent='center'>
               <Avatar
@@ -40,14 +71,20 @@ export function GSideNav({ children }: GSideNavProps) {
                 </ListItemButton>
               </List>
             </Box>
-
           </Box>
         </Drawer>
-        <Box paddingTop={10} height='100vh' marginLeft={theme.spacing(28)}>
+        <Box
+          paddingTop={10}
+          height='100vh'
+          sx={{
+            marginLeft: open ? theme.spacing(30) : 2,
+            transition: 'margin 225ms cubic-bezier(0.4, 0, 0.6, 1)',
+          }}
+        >
           {children}
         </Box>
       </Box>
     </>
-
   );
 }
+
