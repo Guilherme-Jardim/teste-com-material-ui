44e356f886b28e4df6bf0b3a61101bfeef2182a5
Guilherme-Jardim <guilhermejardimj@gmail.com>
2023-11-09 16:33:08 -0300

    drawer

--- app/components/drawer/GDrawer.tsx
+++ app/components/drawer/GDrawer.tsx
@@ -1,14 +1,204 @@
+// import React, { useState, useEffect, useRef, ReactNode } from 'react';
+// import { Box, Drawer, CssBaseline, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, IconButton, useTheme, Collapse } from '@mui/material';
+// import { Menu as MenuIcon, ChevronLeft as ChevronLeftIcon, ChevronRight as ChevronRightIcon } from '@mui/icons-material';
+// import { ExpandLess, ExpandMore } from '@mui/icons-material';
+// import Link from 'next/link';
+
+// interface GDrawerProps {
+//   children: ReactNode;
+//   open: boolean;
+//   toggleDrawer: () => void;
+// }
+
+// const drawerWidth = 240;
+
+// export function GDrawer({ children, open, toggleDrawer }: GDrawerProps) {
+//   const theme = useTheme();
+//   const inboxIconRef = useRef<HTMLDivElement>(null);
+//   const [iconWidth, setIconWidth] = useState(0);
+//   const [submenuOpen, setSubmenuOpen] = useState(false);
+//   const [subsubmenuOpen, setSubsubmenuOpen] = useState(false);
+
+//   useEffect(() => {
+//     const inboxIconElement = inboxIconRef.current;
+//     if (inboxIconElement) {
+//       const inboxIconWidth = getComputedStyle(inboxIconElement).width;
+//       setIconWidth(parseInt(inboxIconWidth));
+//     }
+//   }, []);
+
+//   const handleSubmenuClick = () => {
+//     setSubmenuOpen(!submenuOpen);
+//   };
+
+//   const handleSubsubmenuClick = () => {
+//     setSubsubmenuOpen(!subsubmenuOpen);
+//   };
+
+//   useEffect(() => {
+//     if (!open) {
+//       setSubmenuOpen(false);
+//       setSubsubmenuOpen(false);
+//     }
+//   }, [open]);
+
+//   return (
+//     <Box sx={{ display: 'flex' }}>
+//       <CssBaseline />
+//       <Drawer
+//         variant="permanent"
+//         open={open}
+//         sx={{
+//           flexShrink: 0,
+//           '& .MuiDrawer-paper': {
+//             width: open ? `${drawerWidth}px` : `${iconWidth}px`,
+//             boxSizing: 'border-box',
+//             transition: 'width 0.2s',
+//           },
+//         }}
+//       >
+//         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: open ? 'end' : 'center', paddingTop: '100px' }}>
+//           <IconButton onClick={toggleDrawer}>
+//             {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+//           </IconButton>
+//         </Box>
+//         <Divider />
+//         <List>
+//           <ListItem disablePadding>
+//             <Link href="/">
+//               <ListItemButton alignItems="center">
+//                 <ListItemIcon ref={inboxIconRef}>
+//                   <MenuIcon />
+//                 </ListItemIcon>
+//                 {open && <ListItemText primary="Home" />}
+//               </ListItemButton>
+//             </Link>
+//           </ListItem>
+//           <ListItem button onClick={handleSubmenuClick} >
+//             <ListItemIcon>
+//               <MenuIcon />
+//             </ListItemIcon>
+//             <ListItemText primary="Submenu" />
+//             {submenuOpen ? <ExpandLess /> : <ExpandMore />}
+//           </ListItem>
+//           <Collapse in={submenuOpen} timeout="auto" unmountOnExit>
+//             <List component="div" disablePadding sx={{ marginLeft: '20px' }}>
+//               <ListItem button onClick={handleSubsubmenuClick} sx={{ paddingLeft: '20px' }}>
+//                 <ListItemIcon>
+//                   <MenuIcon />
+//                 </ListItemIcon>
+//                 <ListItemText primary="Subsubmenu" />
+//                 {subsubmenuOpen ? <ExpandLess /> : <ExpandMore />}
+//               </ListItem>
+//               <Collapse in={subsubmenuOpen} timeout="auto" unmountOnExit>
+//                 <List component="div" disablePadding sx={{ marginLeft: '20px' }}>
+//                   <ListItem button>
+//                     <ListItemIcon>
+//                       <MenuIcon />
+//                     </ListItemIcon>
+//                     <ListItemText primary="Subsubmenu Item 1" />
+//                   </ListItem>
+//                   <ListItem button>
+//                     <ListItemIcon>
+//                       <MenuIcon />
+//                     </ListItemIcon>
+//                     <ListItemText primary="Subsubmenu Item 2" />
+//                   </ListItem>
+//                 </List>
+//               </Collapse>
+//             </List>
+//           </Collapse>
+//         </List>
+//       </Drawer>
+
+//       <Box
+//         paddingTop={10}
+//         height='100vh'
+//         sx={{
+//           marginLeft: open ? `calc(${drawerWidth}px + 10px)` : `calc(${iconWidth}px + 10px)`,
+//           transition: 'margin 0.2s',
+//         }}
+//       >
+//         {children}
+//       </Box>
+//     </Box>
+//   );
+// }
+
+
+
+
+
+
+
+
+
 import React, { useState, useEffect, useRef, ReactNode } from 'react';
 import { Box, Drawer, CssBaseline, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, IconButton, useTheme, Collapse } from '@mui/material';
 import { Menu as MenuIcon, ChevronLeft as ChevronLeftIcon, ChevronRight as ChevronRightIcon } from '@mui/icons-material';
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
 import Link from 'next/link';
+import { GMenu } from './GItemList';
 
 interface GDrawerProps {
   children: ReactNode;
   open: boolean;
   toggleDrawer: () => void;
 }
+
+
+const menus = [
+  {
+    menu: "Menu Principal",
+    menutext: "Ir para a Página Principal",
+    menuicon: <ExpandMore />, // Substitua isso pelo ícone real
+    hrefmenu: "#",
+    submenus: [
+      {
+        submenu: "Submenu 1",
+        submenutext: "Ir para Submenu 1",
+        submenuicon: <ExpandMore />, // Substitua pelo ícone real
+        hrefsubmenu: "#",
+        subsubmenus: [
+          {
+            subsubmenu: "Subsubmenu 1",
+            subsubmenutext: "Ir para Subsubmenu 1",
+            subsubmenuicon: <ExpandMore />, // Substitua pelo ícone real
+            hrefsubsubmenu: "#",
+          },
+        ],
+      },
+      // Adicione outros submenus, se necessário
+    ],
+
+  },
+  {
+    menu: "Menu Principal",
+    menutext: "Ir para a Página Principal",
+    menuicon: <ExpandMore />, // Substitua isso pelo ícone real
+    hrefmenu: "/pagina-principal",
+    submenus: [
+      {
+        submenu: "Submenu 1",
+        submenutext: "Ir para Submenu 1",
+        submenuicon: <ExpandMore />, // Substitua pelo ícone real
+        hrefsubmenu: "#",
+        subsubmenus: [
+          {
+            subsubmenu: "Subsubmenu 1",
+            subsubmenutext: "Ir para Subsubmenu 1",
+            subsubmenuicon: <ExpandMore />, // Substitua pelo ícone real
+            hrefsubsubmenu: "#",
+          },
+        ],
+      },
+      // Adicione outros submenus, se necessário
+    ],
+
+  },
+  // Adicione outros menus, se necessário
+];
+
 
 const drawerWidth = 240;
 
@@ -74,42 +264,14 @@ export function GDrawer({ children, open, toggleDrawer }: GDrawerProps) {
               </ListItemButton>
             </Link>
           </ListItem>
-          <ListItem button onClick={handleSubmenuClick} >
-            <ListItemIcon>
-              <MenuIcon />
-            </ListItemIcon>
-            <ListItemText primary="Submenu" />
-            {submenuOpen ? <ExpandLess /> : <ExpandMore />}
-          </ListItem>
-          <Collapse in={submenuOpen} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding sx={{ marginLeft: '20px' }}>
-              <ListItem button onClick={handleSubsubmenuClick} sx={{ paddingLeft: '20px' }}>
-                <ListItemIcon>
-                  <MenuIcon />
-                </ListItemIcon>
-                <ListItemText primary="Subsubmenu" />
-                {subsubmenuOpen ? <ExpandLess /> : <ExpandMore />}
-              </ListItem>
-              <Collapse in={subsubmenuOpen} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding sx={{ marginLeft: '20px' }}>
-                  <ListItem button>
-                    <ListItemIcon>
-                      <MenuIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Subsubmenu Item 1" />
-                  </ListItem>
-                  <ListItem button>
-                    <ListItemIcon>
-                      <MenuIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Subsubmenu Item 2" />
-                  </ListItem>
-                </List>
-              </Collapse>
-            </List>
-          </Collapse>
-        </List>
-      </Drawer>
+          <GMenu
+            menu={menus[0].menu}
+            menutext={menus[0].menutext}
+            menuicon={menus[0].menuicon}
+            hrefmenu={menus[0].hrefmenu}
+            submenus={menus[0].submenus}
+          />        </List>
+      </Drawer >
 
       <Box
         paddingTop={10}
@@ -121,6 +283,6 @@ export function GDrawer({ children, open, toggleDrawer }: GDrawerProps) {
       >
         {children}
       </Box>
-    </Box>
+    </Box >
   );
 }
--- app/components/drawer/GItemList.tsx
+++ app/components/drawer/GItemList.tsx
@@ -1,39 +1,198 @@
-import React, { ReactNode, useEffect, useRef, useState } from 'react';
-import { List, ListItem, ListItemButton, Link, ListItemIcon, ListItemText } from '@mui/material';
+// import { ExpandLess, ExpandMore } from '@mui/icons-material';
+// import { ListItemButton, ListItemIcon, ListItemText, Collapse } from '@mui/material';
+// import { List, ListItem } from '@mui/material';
+// import Link from 'next/link';
+// import React from 'react';
+// import { forwardRef, useState } from 'react';
 
-interface GItemListProps {
-  href: string;
-  icon: ReactNode;
-  text: string;
-  open?: boolean;
-  inboxIconRef?: React.RefObject<HTMLDivElement>; // Adicione a propriedade 'inboxIconRef' com o ponto de interrogação
+// interface GMenuProps {
+//   menu: string;
+//   menutext: string;
+//   menuicon: React.ReactNode;
+//   hrefmenu: string;
+//   submenus: {
+//     submenu: string;
+//     submenutext: string;
+//     submenuicon: React.ReactNode;
+//     hrefsubmenu: string;
+//     subsubmenus: {
+//       subsubmenu: string;
+//       subsubmenutext: string;
+//       subsubmenuicon: React.ReactNode;
+//       hrefsubsubmenu: string;
+//     }[];
+//   }[];
+// }
+
+// export const GMenu = forwardRef(function GMenu(
+//   { menu, menutext, menuicon, submenus, hrefmenu }: GMenuProps,
+//   ref?: React.Ref<HTMLInputElement>
+// ) {
+//   const [submenuOpen, setSubmenuOpen] = useState(false);
+//   const [subsubmenuOpen, setSubsubmenuOpen] = useState<Record<number, boolean>>({});
+
+//   const handleSubmenuClick = () => {
+//     setSubmenuOpen(!submenuOpen);
+//   };
+
+//   const handleSubsubmenuClick = (index: number) => {
+//     setSubsubmenuOpen((prevState) => ({
+//       ...prevState,
+//       [index]: !prevState[index],
+//     }));
+//   };
+
+//   return (
+//     <List>
+//       <Link href={hrefmenu}>
+//         <ListItemButton component="a" href={hrefmenu}>
+//           <ListItemIcon>
+//             {menuicon}
+//           </ListItemIcon>
+//           <ListItemText primary={menutext} />
+//           {submenuOpen ? <ExpandLess /> : <ExpandMore />}
+//         </ListItemButton>
+//       </Link>
+//       <Collapse in={submenuOpen} timeout="auto" unmountOnExit>
+//         <List style={{ paddingLeft: '20px' }}>
+//           {submenus.map((submenu, index) => (
+//             <React.Fragment key={index}>
+//               <Link href={submenu.hrefsubmenu}>
+//                 <ListItemButton component="a" href={submenu.hrefsubmenu} onClick={() => handleSubsubmenuClick(index)} style={{ paddingLeft: '20px' }}>
+//                   <ListItemIcon>
+//                     {submenu.submenuicon}
+//                   </ListItemIcon>
+//                   <ListItemText primary={submenu.submenutext} />
+//                   {subsubmenuOpen[index] ? <ExpandLess /> : <ExpandMore />}
+//                 </ListItemButton>
+//               </Link>
+//               <Collapse in={subsubmenuOpen[index]} timeout="auto" unmountOnExit>
+//                 <List style={{ paddingLeft: '20px' }}>
+//                   {submenu.subsubmenus.map((subsubmenu, subIndex) => (
+//                     <Link href={subsubmenu.hrefsubsubmenu} key={subIndex}>
+//                       <ListItemButton component="a" href={subsubmenu.hrefsubsubmenu}>
+//                         <ListItemIcon>
+//                           {subsubmenu.subsubmenuicon}
+//                         </ListItemIcon>
+//                         <ListItemText primary={subsubmenu.subsubmenutext} />
+//                       </ListItemButton>
+//                     </Link>
+//                   ))}
+//                 </List>
+//               </Collapse>
+//             </React.Fragment>
+//           ))}
+//         </List>
+//       </Collapse>
+//     </List>
+//   );
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useState } from 'react';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
+import { ListItemButton, ListItemIcon, ListItemText, Collapse } from '@mui/material';
+import { List, ListItem } from '@mui/material';
+import Link from 'next/link';
+
+interface GMenuProps {
+  menu: string;
+  menutext: string;
+  menuicon: React.ReactNode;
+  hrefmenu: string;
+  submenus: {
+    submenu: string;
+    submenutext: string;
+    submenuicon: React.ReactNode;
+    hrefsubmenu: string;
+    subsubmenus: {
+      subsubmenu: string;
+      subsubmenutext: string;
+      subsubmenuicon: React.ReactNode;
+      hrefsubsubmenu: string;
+    }[];
+  }[];
 }
 
-export function GItemList({ href, icon, text, open, inboxIconRef }: GItemListProps) {
-  const [iconWidth, setIconWidth] = useState(0);
 
-  useEffect(() => {
-    if (inboxIconRef) {
-      const inboxIconElement = inboxIconRef.current;
-      if (inboxIconElement) {
-        const inboxIconWidth = getComputedStyle(inboxIconElement).width;
-        setIconWidth(parseInt(inboxIconWidth));
-      }
-    }
-  }, [inboxIconRef]);
+
+
+
+
+export function GMenu({ menu, menutext, menuicon, hrefmenu, submenus }: GMenuProps) {
+  const [submenuOpen, setSubmenuOpen] = useState(false);
+  const [subsubmenuOpen, setSubsubmenuOpen] = useState(false);
+
+  const handleSubmenuClick = () => {
+    setSubmenuOpen(!submenuOpen);
+  };
+
+  const handleSubsubmenuClick = () => {
+    setSubsubmenuOpen(!subsubmenuOpen);
+  };
 
   return (
-    <ListItem disablePadding>
-      <Link style={{
-        width: '100%', color: 'initial', textDecoration: 'none',
-      }} href={href}>
-        <ListItemButton alignItems="center">
-          <ListItemIcon ref={inboxIconRef}>
-            {icon}
+    <>
+      {/* Início do map do menu */}
+      <Link href={hrefmenu}>
+        <ListItem style={{ height: '50px' }} button onClick={handleSubmenuClick}>
+          <ListItemIcon>
+            {menuicon}
           </ListItemIcon>
-          {open && <ListItemText primary={text} />}
-        </ListItemButton>
+          <ListItemText primary={menutext} />
+          {submenuOpen ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
       </Link>
-    </ListItem>
+      {/* Fim do map do menu */}
+      <Collapse in={submenuOpen} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding sx={{ paddingLeft: '20px' }}>
+          {/* Map do submenu */}
+          {submenus.map((submenu, index) => (
+            <React.Fragment key={index}>
+              <Link href={submenu.hrefsubmenu}>
+                <ListItem style={{ height: '50px' }} button onClick={handleSubsubmenuClick} sx={{ paddingLeft: '20px' }}>
+                  <ListItemIcon>
+                    {submenu.submenuicon}
+                  </ListItemIcon>
+                  <ListItemText primary={submenu.submenutext} />
+                  {subsubmenuOpen ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+              </Link>
+              {/* Fim do map do submenu */}
+              <Collapse in={subsubmenuOpen} timeout="auto" unmountOnExit>
+                <List component="div" sx={{ paddingLeft: '20px' }}>
+                  {/* Map do subsubmenu */}
+                  {submenu.subsubmenus.map((subsubmenu, index) => (
+                    <Link href={subsubmenu.hrefsubsubmenu} key={index}>
+                      <ListItem style={{ height: '50px' }}>
+                        <ListItemIcon>
+                          {subsubmenu.subsubmenuicon}
+                        </ListItemIcon>
+                        <ListItemText primary={subsubmenu.subsubmenutext} />
+                      </ListItem>
+                    </Link>
+                  ))}
+                  {/* Fim do map do subsubmenu */}
+                </List>
+              </Collapse>
+            </React.Fragment>
+          ))}
+        </List>
+      </Collapse>
+    </>
   );
 }
